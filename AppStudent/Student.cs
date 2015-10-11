0cb5f22710dcea3215ebf43d8582a4a307b7becd
SpoilingCode <spoilingcode@mail.ru>
2015-10-11 16:06:15 +0300

    init

---
+++ AppStudent/Student.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStudent
+{
+    class Student
+    {
+        static public int quantityObjects = 0;
+
+        string name;
+        string lastName;
+        string patronymic;
+        int day_birth;
+        int month_birth;
+        int year_birth;
+
+
+        public Student()
+        {
+
+            name = "Unknown";
+            lastName = "Unknown";
+            patronymic = "Unknown";
+            day_birth = 0;
+            month_birth = 0;
+            year_birth = 0;
+            quantityObjects++;
+
+        }
+
+
+        public Student(string _lastName)
+            : this()
+        {
+
+            if (_lastName != "") lastName = _lastName;
+            else lastName = "Unknown";
+
+        }
+
+        public Student(string name, string patronymic, string lastName, int day_birth, int month_birth, int year_birth)
+        {
+
+            if (name != "")
+            {
+
+                this.name = name;
+
+            }
+            else name = "Unknown";
+
+            if (lastName != "")
+            {
+
+                this.lastName = lastName;
+
+            }
+            else lastName = "Unknown";
+
+
+            if (patronymic != "")
+            {
+
+                this.patronymic = patronymic;
+
+            }
+            else patronymic = "Unknown";
+
+            if (day_birth >= 1 && day_birth <= 31)
+            {
+
+                this.day_birth = day_birth;
+
+            }
+            else throw new Exception("День рождения указан неверно.Значение должно быть в интервале [1, 31]");
+
+            if (month_birth >= 1 && month_birth <= 12)
+            {
+
+                this.month_birth = month_birth;
+
+            }
+            else throw new Exception("Месяц рождения указан неверно.Значение должно быть в интервале [1, 12]");
+
+            if (year_birth >= 1985 && year_birth <= 2015)
+            {
+
+                this.year_birth = year_birth;
+
+            }
+            else throw new Exception("Год рождения указан неверно.Значение должно быть в интервале [1985, 2015]");
+
+            quantityObjects++;
+
+        }
+
+
+        public string Name
+        {
+
+            get { return name; }
+
+            set
+            {
+                if (name == "Unknown")
+                {
+
+                    if (value == "")
+                    {
+                        name = "Unknown";
+                    }
+                    else name = value;
+
+                }
+                else throw new Exception("Имя студента задано и не может быть изменено данным способом.");
+
+            }
+
+        }
+
+        public string LastName
+        {
+
+            get { return lastName; }
+
+            set
+            {
+                if (lastName == "Unknown")
+                {
+
+                    if (value == "")
+                    {
+                        lastName = "Unknown";
+                    }
+                    else lastName = value;
+
+                }
+                else throw new Exception("Фамилия студента задана и не может быть изменено данным способом.");
+
+            }
+
+        }
+
+        public string Patronymic
+        {
+
+            get { return patronymic; }
+
+            set
+            {
+                if (patronymic == "Unknown")
+                {
+
+                    if (value == "")
+                    {
+                        patronymic = "Unknown";
+                    }
+                    else patronymic = value;
+
+                }
+                else throw new Exception("Отчество студента задано и не может быть изменено данным способом.");
+
+            }
+
+        }
+
+        public int Day_birth
+        {
+            get { return day_birth; }
+
+            set
+            {
+
+                if (value >= 1 && value <= 31)
+                {
+                    day_birth = value;
+                }
+                else throw new Exception("День рождения указан неверно.Значение должно быть в интервале [1, 31]");
+
+            }
+
+        }
+
+        public int Month_birth
+        {
+            get { return month_birth; }
+
+            set
+            {
+
+                if (value >= 1 && value <= 12)
+                {
+                    month_birth = value;
+                }
+                else throw new Exception("Месяц рождения указан неверно.Значение должно быть в интервале [1, 12]");
+
+            }
+
+        }
+
+        public int Year_birth
+        {
+            get { return year_birth; }
+
+            set
+            {
+
+                if (value >= 1985 && value <= 2015)
+                {
+                    year_birth = value;
+                }
+                else throw new Exception("Год рождения указан неверно.Значение должно быть в интервале [1985, 2015]");
+
+            }
+
+        }
+
+
+        public void inputDataAboutStudents()
+        {
+
+            Console.WriteLine("Введите имя cтудента:");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Введите отчество студента:");
+            Patronymic = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию студента:");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите день рождения студента:");
+            day_birth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите месяц рождения студента:");
+            month_birth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите год рождения студента:");
+            year_birth = int.Parse(Console.ReadLine());
+
+        }
+
+        public void outputDataAboutStudents()
+        {
+
+            Console.Write(" ФИО: {0} {1} {2}  Дата Рождения: {3}.{4}.{5}\n", lastName, name, patronymic, day_birth, month_birth, year_birth);
+        }
+
+
+
+        public static void findStudentsByYearBirth(Student[] arrStudents, int numbersStudents, int readYearBirth)
+        {
+
+            if (numbersStudents == 0) { throw new Exception("\nВ списке нет студентов"); }
+            bool flag = false;
+            for (int i = 0; i < numbersStudents; i++)
+            {
+                if (arrStudents[i].Year_birth == readYearBirth)
+                {
+                    arrStudents[i].outputDataAboutStudents();
+                    flag = true;
+                }
+            }
+
+            if (flag == false)
+            {
+                Console.WriteLine("В списке нет студентов с годом рождения:{0}", readYearBirth);
+            }
+
+        }
+
+
+        public static int operator -(Student firstStudent, Student secondStudent)
+        {
+            DateTime dateBirthFirstStudent = new DateTime(firstStudent.Year_birth, firstStudent.Month_birth, firstStudent.Day_birth);
+            DateTime dateBirthSecondStudent = new DateTime(secondStudent.Year_birth, secondStudent.Month_birth, secondStudent.Day_birth);
+
+            TimeSpan timeSpan = dateBirthFirstStudent - dateBirthSecondStudent;
+
+            return timeSpan.Days;
+        }
+
+    }
+}
